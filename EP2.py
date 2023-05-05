e5da77b096005828be537078504d8ab8604f5548
Joao Pedro Brand Guimaraes <joaopbg1@al.insper.edu.br>
2023-05-05 15:13:25 -0300

    preenche frota

--- EP2.py
+++ EP2.py
@@ -7,3 +7,20 @@ def define_posicoes(linha,coluna,orientacao,tamanho):
         for i in range (0,tamanho):
             lista_f.append([linha +i, coluna])
     return lista_f
+
+def define_posicoes(linha,coluna,orientacao,tamanho):
+    lista_f=[]
+    if orientacao == 'horizontal':
+        for i in range (0,tamanho):
+            lista_f.append([linha, coluna +i])
+    elif orientacao =='vertical':
+        for i in range (0,tamanho):
+            lista_f.append([linha +i, coluna])
+    return lista_f
+    
+def preenche_frota(frota,nome_navio,linha,coluna,orientacao,tamanho):
+    if nome_navio not in frota.keys():
+        frota[nome_navio] = []
+    frota[nome_navio].append(define_posicoes(linha, coluna, orientacao, tamanho))
+    return frota
+    
