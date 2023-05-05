74777d687dc8d8d4fad9409ddd3c7b5efd3a7fca
Joao Pedro Brand Guimaraes <joaopbg1@al.insper.edu.br>
2023-05-05 15:09:03 -0300

    define posiçoes

--- dev1.py
+++ dev1.py
@@ -1,1 +1,9 @@
-print ('Joao Pedro Guimaraes')
+def define_posicoes(linha,coluna,orientacao,tamanho):
+    lista_f=[]
+    if orientacao == 'horizontal':
+        for i in range (0,tamanho):
+            lista_f.append([linha, coluna +i])
+    elif orientacao =='vertical':
+        for i in range (0,tamanho):
+            lista_f.append([linha +i, coluna])
+    return lista_f
