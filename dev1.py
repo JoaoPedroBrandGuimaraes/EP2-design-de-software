d6aa38c6b3bcf2f26c01e66627c1c77eb439ee57
Joao Pedro Brand Guimaraes <joaopbg1@al.insper.edu.br>
2023-05-08 22:18:41 -0300

    primeiro commit do Joao Pedro

--- dev1.py
+++ dev1.py
@@ -1,9 +1,1 @@
-def define_posicoes(linha,coluna,orientacao,tamanho):
-    lista_f=[]
-    if orientacao == 'horizontal':
-        for i in range (0,tamanho):
-            lista_f.append([linha, coluna +i])
-    elif orientacao =='vertical':
-        for i in range (0,tamanho):
-            lista_f.append([linha +i, coluna])
-    return lista_f
+print(Joao Pedro Guimaraes)
